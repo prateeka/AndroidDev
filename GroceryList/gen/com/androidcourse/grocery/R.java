494237e80834e870a71f81da7521d1fade615945
Paryanz <paryanz@gmail.com>
2011-12-13 22:58:15 -0800

    Submitted - This is the code for submitted project Changes comapred to teh prior version include introducing icons, themes.

--- GroceryList/gen/com/androidcourse/grocery/R.java
+++ GroceryList/gen/com/androidcourse/grocery/R.java
@@ -11,37 +11,41 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int groceryicon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int clearButton=0x7f06000b;
-        public static final int editText1=0x7f060002;
-        public static final int editText2=0x7f060005;
-        public static final int editText3=0x7f060008;
-        public static final int lablel1=0x7f060001;
-        public static final int lablel2=0x7f060004;
-        public static final int lablel3=0x7f060007;
-        public static final int linearLayout1=0x7f060000;
-        public static final int linearLayout2=0x7f060003;
-        public static final int linearLayout3=0x7f060006;
-        public static final int linearLayout4=0x7f060009;
-        public static final int listView1=0x7f06000d;
-        public static final int menuGroup_Main=0x7f06000e;
-        public static final int menu_AddItem=0x7f06000f;
-        public static final int operationButton=0x7f06000a;
-        public static final int spinner1=0x7f06000c;
+        public static final int clearButton=0x7f07000b;
+        public static final int editText1=0x7f070002;
+        public static final int editText2=0x7f070005;
+        public static final int editText3=0x7f070008;
+        public static final int lablel1=0x7f070001;
+        public static final int lablel2=0x7f070004;
+        public static final int lablel3=0x7f070007;
+        public static final int linearLayout1=0x7f070000;
+        public static final int linearLayout2=0x7f070003;
+        public static final int linearLayout3=0x7f070006;
+        public static final int linearLayout4=0x7f070009;
+        public static final int listView1=0x7f07000d;
+        public static final int menuGroup_Main=0x7f07000e;
+        public static final int menu_AddItem=0x7f07000f;
+        public static final int operationButton=0x7f07000a;
+        public static final int spinner1=0x7f07000c;
     }
     public static final class layout {
         public static final int additem=0x7f030000;
         public static final int displaytraderitem=0x7f030001;
     }
     public static final class menu {
-        public static final int displaytraderitemmenu=0x7f050000;
+        public static final int displaytraderitemmenu=0x7f060000;
     }
     public static final class string {
         public static final int ADD_BUTTON_LABEL=0x7f040001;
         public static final int CONTEXT_MENU_DELETE=0x7f040003;
         public static final int UPDATE_BUTTON_LABEL=0x7f040002;
         public static final int app_name=0x7f040000;
+    }
+    public static final class style {
+        public static final int SampleTextTheme=0x7f050000;
     }
 }
