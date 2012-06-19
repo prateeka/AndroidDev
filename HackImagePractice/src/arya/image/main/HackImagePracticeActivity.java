1e449b0710f4b7d62f2b9b9f3c4a1d4080be186e
Prateek Asthana <paryanz@gmail.com>
2012-06-19 15:22:51 -0700

    HackPracticeImage-Initial - Initial version

---
+++ HackImagePractice/src/arya/image/main/HackImagePracticeActivity.java
@@ -0,0 +1,13 @@
+package arya.image.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HackImagePracticeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
