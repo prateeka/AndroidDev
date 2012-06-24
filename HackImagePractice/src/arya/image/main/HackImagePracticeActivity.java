ff5117b53bdc9f73fd48fc25d924d9feccc8f9b4
Prateek Asthana <paryanz@gmail.com>
2012-06-23 20:02:42 -0700

    HackPracticeImage: Working - ImageProcessor updated to return Image URI
    Next step: introduce functionality for loading image from camera.

--- HackImagePractice/src/arya/image/main/HackImagePracticeActivity.java
+++ HackImagePractice/src/arya/image/main/HackImagePracticeActivity.java
@@ -1,15 +1,8 @@
 package arya.image.main;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,8 +10,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
-public class HackImagePracticeActivity extends Activity {
-	private static final int SELECT_IMAGE = 1;
+public class HackImagePracticeActivity extends Activity implements
+		OnClickListener {
 	private ImageProcessor imageProcessor;
 	private ImageView imageView;
 	
@@ -27,15 +20,15 @@ public class HackImagePracticeActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		imageProcessor = new ImageProcessor();
+		imageProcessor = new ImageProcessor(this);
 		imageView = (ImageView) findViewById(R.id.imageView);
-		imageView.setOnClickListener(imageProcessor);
+		imageView.setOnClickListener(this);
 	}
 	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if ((requestCode == SELECT_IMAGE) && (resultCode == Activity.RESULT_OK)) {
-			imageProcessor.onActivityResult(data);
+		if (resultCode == Activity.RESULT_OK) {
+			imageProcessor.loadPicture(requestCode, data);
 		}
 	}
 	
@@ -57,76 +50,17 @@ public class HackImagePracticeActivity extends Activity {
 		return handled;
 	}
 	
-	private class ImageProcessor implements OnClickListener {
-		private static final String TAG = "ImageClickListener";
-		
-		@Override
-		public void onClick(View v) {
-			Log.d(TAG, "imageView clicked");
-			Intent imageChooser = new Intent(Intent.ACTION_GET_CONTENT);
-			imageChooser.setType("image/*");
-			startActivityForResult(imageChooser, SELECT_IMAGE);
-		}
-		
-		public boolean flipImage() {
-			imageView.setRotation(imageView.getRotation() + 90);
-			return true;
-		}
-		
-		public void onActivityResult(Intent data) {
-			
-			Uri uri = data.getData();
-			if (uri == null) {
-				Log.d(TAG, "data.getData() is null");
-			} else {
-				setPicFromUri(uri);
-			}
-		}
-		
-		private void setPicFromUri(Uri uri) {
-			imageView.setRotation(0);
-			
-			try {
-				// Get the dimensions of the View
-				int targetW = imageView.getWidth();
-				int targetH = imageView.getHeight();
-				
-				// Get the dimensions of the bitmap
-				BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-				bmOptions.inJustDecodeBounds = true;
-				
-				InputStream inputStream = getContentResolver().openInputStream(
-						uri);
-				
-				BitmapFactory.decodeStream(inputStream, null, bmOptions);
-				inputStream.close();
-				
-				int photoW = bmOptions.outWidth;
-				int photoH = bmOptions.outHeight;
-				
-				// Determine how much to scale down the image
-				int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
-				
-				// Decode the image file into a Bitmap sized to fill the View
-				bmOptions.inJustDecodeBounds = false;
-				bmOptions.inSampleSize = scaleFactor;
-				bmOptions.inPurgeable = true;
-				
-				inputStream = getContentResolver().openInputStream(uri);
-				Bitmap bitmap = BitmapFactory.decodeStream(
-						inputStream,
-						null,
-						bmOptions);
-				if (bitmap == null) {
-					Log.d(TAG, "bitmap is null ");
-				} else {
-					imageView.setImageBitmap(bitmap);
-				}
-				inputStream.close();
-			}
-			catch (IOException ex) {
-				Log.e(TAG, "exception :" + ex);
-			}
-		}
+	@Override
+	public void onClick(View v) {
+		imageProcessor.initiatePictureLoad(ImageProcessor.LOAD_FROM_GALLERY);
 	}
+	
+	ImageView getImageView() {
+		return imageView;
+	}
+	
+	void setImageView(ImageView imageView) {
+		this.imageView = imageView;
+	}
+	
 }
